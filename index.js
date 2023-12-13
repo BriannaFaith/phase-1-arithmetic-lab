a619c89216a9716775022937982bf1b6fe33bece
Faith <faithbrianna01@gmail.com>
2023-12-13 08:48:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+ const num1= 2 ;
+ const num2= 31;
+
+ const multiply= num1 * num2 ;
+
+const random = Math.floor(Math.random() *100) +1;
+
+
+const num3= 19 ;
+const num4= 5 ;
+ const mod = num3 % num4 ;
+
+const numBer1= 5;
+const numBer2= 10;
+const numBer3 = 15;
+const numBer4 = 20;
+ const max = Math.max (numBer1,numBer2,numBer3,numBer4);
